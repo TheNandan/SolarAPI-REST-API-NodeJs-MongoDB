8c92a1b0473eb4ff6ddb6385c49dc65f6feb095f
Nandan S <thenandansrinivas@outlook.com>
2022-12-19 21:41:00 +0530

    mongoDB connection and Schema creation

--- server.js
+++ server.js
@@ -4,13 +4,31 @@ const bodyparser = require('body-parser');
 const morgan = require('morgan');
 const ejs = require('ejs');
 const path = require('path');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 
 
-
+// server
 const app = express();
 
+// server port
 const PORT = process.env.PORT || 8080;
 
+// mongoose connection
+const DB = process.env.DB || 'solarLDB';
+mongoose.connect(`mongodb://127.0.0.1:27017/${DB}`,{useNewUrlParser:true});
+
+// collection schema`
+const planetSchema = {
+    'position':Number,
+    'name':String,
+    'velocity':Number,
+    'distance':Number,
+    'description':String
+};
+const Planet = mongoose.model("Planet",planetSchema);
+
+// middleware
 app.use(bodyparser.urlencoded({extended:true}));
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(morgan('tiny'));
